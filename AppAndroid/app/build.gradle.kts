8279786d09fce7556f9d604d00932f53ce016477
SenpaiChry <christiancolosio86@gmail.com>
2026-09-15 17:03:55 +0200

    Bump versione 0.2 (versionCode 3) per test aggiornamento in-app

--- AppAndroid/app/build.gradle.kts
+++ AppAndroid/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.teammaker.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.1"
+        versionCode = 3
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
